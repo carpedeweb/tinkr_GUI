7432953b95a6b1a019b19ed66624340e3330bd62
carpedeweb <94760373+carpedeweb@users.noreply.github.com>
2022-01-07 13:22:49 -0600

    Add files via upload

---
+++ python_updated_gui.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+def tinkr():
+
+    window = Tk()
+    window.geometry("300x250")
+    window.title("Account Login")
+
+    l1=Label(window, text="Username:").pack()
+    l1_entry=Entry(window, show="", textvariable=StringVar()).pack()
+    l2=Label(window, text='Password:').pack()
+    t1=Entry(window, show='*', textvariable=StringVar()).pack()
+    b1=Button(window, text="Login").pack()
+
+    window.mainloop()
+tinkr()
+    
